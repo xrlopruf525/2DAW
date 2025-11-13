d6d77fe9a9dfd61dcf1e0f5fd24751498b7a6753
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-11-13 14:47:01 +0100

    Clase Jueves

--- DesarrolloCliente-Front-main/Tema2.HTML/Boletín1/EJ07/ej07.js
+++ DesarrolloCliente-Front-main/Tema2.HTML/Boletín1/EJ07/ej07.js
@@ -1,1 +1,14 @@
+let teclado = document.getElementById("teclado");
+let salida = document.getElementById("salida");
 
+teclado.addEventListener("click", mostrarSalida);
+
+function mostrarSalida(event) {
+  if (event.target.tagName === "INPUT") {
+    salida.value += event.target.value;
+  }
+
+
+
+  
+}
