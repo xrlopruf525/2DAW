da8e2608cd69df73b0f980a26ea677b6639121b6
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-11-14 11:20:01 +0100

    Ej 12

--- DesarrolloCliente-Front-main/Tema2.HTML/Boletín1/EJ12/ej12.js
+++ DesarrolloCliente-Front-main/Tema2.HTML/Boletín1/EJ12/ej12.js
@@ -1,1 +1,9 @@
 
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(event){
+    if(formulario.txtTexto.value.length == 0){
+        event.preventDefault();
+        alert("Texto Vacio")
+    }
+}
