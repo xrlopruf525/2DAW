f1784cbf2980d92aa8dfe3ee2c0f1ee564daf6c3
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-11-19 14:00:46 +0100

    Excepciones

--- SegundoProyecto-Recetas/recetasapp/models.py
+++ SegundoProyecto-Recetas/recetasapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 # class CategoriaChoices(models.TextChoices):
 #     LEGUMBRES = "LE", "Legumbres"
@@ -10,12 +12,20 @@ class CategoriaIngrediente(models.Model):
     def __str__(self):
         return f'{self.nombre}'
     
-
+def validate_nombre(value):
+    if value.lower() == "verdura":
+        raise ValidationError("El nombre no puede ser una categoría")
     
 class Ingredientes(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, validators=[validate_nombre])
     categoria = models.ForeignKey('CategoriaIngrediente', on_delete=models.CASCADE)
     refrigerado = models.BooleanField(default=False)
+
+    def clean(self):
+        if self.nombre.lower() == "verdura":
+            raise ValidationError("El nombre no puede ser una categoría")
+        if self.categoria.nombre.lower() in ["prohibida", "genérica"]:
+            raise ValidationError(f"La categoría '{self.categoria.nombre}' no está permitida")
 
     def __str__(self):
         return f'{self.nombre} ({self.categoria})'
