0da54b6c5d6ed14ba1a2fafff6c5fd5aefc7398f
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-12-04 10:50:39 +0100

    Vista de crud y eliminar cambiado

--- SegundoProyecto-Recetas/recetasapp/views.py
+++ SegundoProyecto-Recetas/recetasapp/views.py
@@ -2,61 +2,71 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
 from django.forms import formset_factory, modelformset_factory
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def inicio(request):
     return render(request, 'recetasapp/inicio.html')
 
-def ingredientes_lista(request):
-    ingredientes = Ingredientes.objects.all()
-    categorias = CategoriaIngrediente.objects.all()
+# def ingredientes_lista(request):
+#     ingredientes = Ingredientes.objects.all()
+#     categorias = CategoriaIngrediente.objects.all()
 
-    categoria_filtro = request.GET.get('categoria')
-    if categoria_filtro:
-        ingredientes = ingredientes.filter(categoria=categoria_filtro)
+#     categoria_filtro = request.GET.get('categoria')
+#     if categoria_filtro:
+#         ingredientes = ingredientes.filter(categoria=categoria_filtro)
 
-    refrigerado_filtro = request.GET.get('refrigerado')
-    if refrigerado_filtro:  
-        ingredientes = ingredientes.filter(refrigerado=True)
+#     refrigerado_filtro = request.GET.get('refrigerado')
+#     if refrigerado_filtro:  
+#         ingredientes = ingredientes.filter(refrigerado=True)
 
-    contexto = {'ingredientes': ingredientes, 'categorias':categorias}
+#     contexto = {'ingredientes': ingredientes, 'categorias':categorias}
 
-    return render(request, 'recetasapp/ingredientes_lista.html', contexto)
+#     return render(request, 'recetasapp/ingredientes_lista.html', contexto)
+
+class IngredienteListView(ListView):
+    model = Ingredientes
+    template_name = 'recetasapp/ingredientes_lista.html'
+    context_object_name = 'ingredientes'
 
 def categoria_lista(request):
     categorias = CategoriaIngrediente.objects.all()
-
-
     contexto = {'categorias':categorias}
-
     return render(request, 'recetasapp/categoria_lista.html', contexto)
 
 
-def ingrediente_crear(request):
-    SetIngredienteFormSet = modelformset_factory(Ingredientes, form=SetIngredienteForm, extra=3)
+# def ingrediente_crear(request):
+#     SetIngredienteFormSet = modelformset_factory(Ingredientes, form=SetIngredienteForm, extra=3)
 
-    if request.method == 'POST':
-        formset = SetIngredienteFormSet(request.POST)
-        if formset.is_valid():
-            # for form in formset:
-            #     if form.cleaned_data:
-            #         nombre = form.cleaned_data.get('nombre')
-            #         categoria = form.cleaned_data.get('categoria')
-            #         refrigerado = form.cleaned_data.get('refrigerado')
+#     if request.method == 'POST':
+#         formset = SetIngredienteFormSet(request.POST)
+#         if formset.is_valid():
+#             # for form in formset:
+#             #     if form.cleaned_data:
+#             #         nombre = form.cleaned_data.get('nombre')
+#             #         categoria = form.cleaned_data.get('categoria')
+#             #         refrigerado = form.cleaned_data.get('refrigerado')
 
                    
-            #         Ingredientes.objects.create(
-            #             nombre=nombre,
-            #             categoria=categoria,
-            #             refrigerado=refrigerado
-            #         )  ESTE ES EL FORMSET
-            formset.save() # ESTO ES PARA EL MODELFORMSET
-            return redirect('ingredientes_lista')
-    else:
-        formset = SetIngredienteFormSet(queryset=Ingredientes.objects.none())
+#             #         Ingredientes.objects.create(
+#             #             nombre=nombre,
+#             #             categoria=categoria,
+#             #             refrigerado=refrigerado
+#             #         )  ESTE ES EL FORMSET
+#             formset.save() # ESTO ES PARA EL MODELFORMSET
+#             return redirect('ingredientes_lista')
+#     else:
+#         formset = SetIngredienteFormSet(queryset=Ingredientes.objects.none())
 
-    estado = 'crear'
-    return render(request, 'recetasapp/ingrediente_form.html', {'formset': formset, 'estado': estado})
+#     estado = 'crear'
+#     return render(request, 'recetasapp/ingrediente_form.html', {'formset': formset, 'estado': estado})
+
+class IngredienteNuevoView(CreateView):
+    model = Ingredientes
+    form_class = IngredienteForm
+    template_name = 'recetasapp/ingrediente_form.html'
+    success_url = reverse_lazy('ingredientes_lista')
 
 def ingrediente_editar(request, pk):
     SetIngredienteFormSet = modelformset_factory(Ingredientes, form=SetIngredienteForm, extra=0)
@@ -73,9 +83,21 @@ def ingrediente_editar(request, pk):
     estado = 'editar'
     return render(request, 'recetasapp/ingrediente_form.html', {'formset': formset, 'estado': estado})
 
-def ingrediente_detalle(request, pk):
-    ingrediente = get_object_or_404(Ingredientes, pk=pk)
-    return render(request, 'recetasapp/ingrediente_detalle.html', {'ingrediente': ingrediente})
+class IngredienteEditarView(UpdateView):
+    model = Ingredientes
+    form_class = IngredienteForm
+    template_name = 'recetasapp/ingrediente_form.html'
+    success_url = reverse_lazy('ingredientes_lista')
+
+# def ingrediente_detalle(request, pk):
+#     ingrediente = get_object_or_404(Ingredientes, pk=pk)
+#     return render(request, 'recetasapp/ingrediente_detalle.html', {'ingrediente': ingrediente})
+
+class IngredienteDetailView(DetailView):
+    model = Ingredientes
+    template_name = 'recetasapp/ingrediente_detalle.html'
+    context_object_name = 'ingrediente'
+
 
 def ingrediente_eliminar(request, pk):
     ingrediente = get_object_or_404(Ingredientes, pk=pk)
@@ -84,13 +106,17 @@ def ingrediente_eliminar(request, pk):
         return redirect('ingredientes_lista')
     return render(request, 'recetasapp/ingrediente_eliminar.html', {'ingrediente': ingrediente})
 
+class IngredienteEliminarView(DeleteView):
+    model = Ingredientes
+    template_name = 'recetasapp/ingrediente_eliminar.html'    
+    success_url = reverse_lazy('ingredientes_lista')
+
 
 def relaciones(request):
     recetas = Receta.objects.all()
     ingredientes = Ingredientes.objects.all()
 
     return render(request, 'recetasapp/relaciones.html', {'recetas':recetas, 'ingredientes':ingredientes})
-
 
 def receta_detalle(request, pk):
     receta = get_object_or_404(Receta, pk=pk)
