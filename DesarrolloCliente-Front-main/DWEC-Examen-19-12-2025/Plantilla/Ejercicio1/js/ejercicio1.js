fb306e047e17886131b4a0493883f58b0deeff2d
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-12-19 11:16:50 +0100

    Ejercicio 1 mal hecho

--- DesarrolloCliente-Front-main/DWEC-Examen-19-12-2025/Plantilla/Ejercicio1/js/ejercicio1.js
+++ DesarrolloCliente-Front-main/DWEC-Examen-19-12-2025/Plantilla/Ejercicio1/js/ejercicio1.js
@@ -1,1 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () { 
+
+
+    const activar = document.querySelector("[name='interviewMode']").checked;    
+
+    if (activar){
+        let imagenes = document.querySelectorAll(".profile-card"); 
+
+          for (let i = 0; i < imagenes.length; i++) {
+
+                const div=document.createElement("div");
+                div.classList.add("bio-info");
+
+                imagen = document.getElementsByTagName("img")
+                const texto = imagen[i].getAttribute("data-bio");
+                div.textContent = texto;
+                imagen[i].append(div)
+
+                imagen[i].addEventListener("mouseenter", cursorDentro(div));
+                imagen[i].addEventListener("mouseenter", cursorFuera(div));
+            }
+        
+    }
+
+    function cursorDentro(div){
+        div.style.display="flex"
+    }
+    function cursorFuera(div){
+        div.style.display="none"
+    }
+
+
+})
 
