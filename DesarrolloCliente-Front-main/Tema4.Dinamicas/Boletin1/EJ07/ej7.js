fd513712c92efef5f9b61048dd086adb11afd922
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2026-01-13 13:00:01 +0100

    sumar 7

--- DesarrolloCliente-Front-main/Tema4.Dinamicas/Boletin1/EJ07/ej7.js
+++ DesarrolloCliente-Front-main/Tema4.Dinamicas/Boletin1/EJ07/ej7.js
@@ -1,7 +1,6 @@
 const sumar = (...numeros) => {
-  return numeros.reduce((acumulador, valor) => acumulador + valor, 0);
+  return numeros.reduce((num, suma) => num + suma, 0);
 };
-
 
 console.log(sumar(2, 5, 10, 3)); // 20
 console.log(sumar(1, 2, 3, 4, 5)); // 15
